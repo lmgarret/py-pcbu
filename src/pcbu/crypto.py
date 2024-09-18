58086e5b0b40d18758602493891f92d0253e91eb
LM. Garret <lm@codingarret.dev>
2024-09-18 14:58:50 +0200

    Variabilize crypto magic numbers

--- src/pcbu/crypto.py
+++ src/pcbu/crypto.py
@@ -8,7 +8,9 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 AES_KEY_SIZE = 256
 IV_SIZE = 16
 SALT_SIZE = 16
+GCM_TAG_SIZE = 16
 ITERATIONS = 65535
+TIMESTAMP_SIZE = 8
 TIMESTAMP_TIMEOUT = 20_000
 
 def current_time_millis() -> int:
@@ -41,15 +43,15 @@ def encrypt_aes(src: bytes, pwd: str) -> bytes:
 def decrypt_aes(src: bytes, pwd: str) -> bytes:
     iv = src[:IV_SIZE]
     salt = src[IV_SIZE:IV_SIZE + SALT_SIZE]
-    ciphertext = src[IV_SIZE + SALT_SIZE:-16]
-    tag = src[-16:]
+    ciphertext = src[IV_SIZE + SALT_SIZE:-GCM_TAG_SIZE]
+    tag = src[-GCM_TAG_SIZE:]
 
     key = generate_key(pwd, salt)
     cipher = Cipher(algorithms.AES(key), modes.GCM(iv, tag))
     decryptor = cipher.decryptor()
     plaintext = decryptor.update(ciphertext) + decryptor.finalize()
-    timestamp = int.from_bytes(plaintext[:8], byteorder="big")
+    timestamp = int.from_bytes(plaintext[:TIMESTAMP_SIZE], byteorder="big")
     time_diff = current_time_millis() - timestamp
     if (time_diff < -TIMESTAMP_TIMEOUT or time_diff > TIMESTAMP_TIMEOUT):
         raise Exception("Invalid timestamp on AES data!")
-    return plaintext[8:]
+    return plaintext[TIMESTAMP_SIZE:]
